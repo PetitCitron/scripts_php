415b9ad1112fb5e2d24f9beaaca763630b8c5592
PetitCitron <contact@lab2prod.com>
2021-02-16 17:11:29 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ensemble de petits script et utilitaires PHP
+
+Les scripts appellents souvent des commandes linux.
+Cela m'évites de coder en bash que j'aime pas et j'ai remplacé python par php pour tous mes usages donc aussi l'admin sys.
---
+++ changement_extension_fichier_en_masse.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Script pour rapidement renommer les extensions de fichiers
+ * d'un repertoire et des ses sous repertoire
+ *
+ *
+ * Utilise la commande linux "find"
+ *
+ * Utilisation :
+ *      copier changement_extension_fichier_en_masse.php dans le repertoire ou renommer les fichiers
+ *      editer les variables $old et $new
+ *      php7.4 changement_extension_fichier_en_masse.php
+ */
+
+$old = '.txt';
+$new = '.md';
+//--------------------------------------------------------------
+$files = [];
+exec('find ./ -name "*'. $old . '"', $files);
+$root = $_SERVER['PWD'] . '/';
+print "Starting renaming all $old into $new\n";
+foreach ($files as $fctp) {
+    $info = pathinfo($fctp);
+    $src = $root . $fctp;
+    $dest =  $root . $info['dirname'] . '/' . $info['filename'] . $new;
+    print 'move : ' . $src . "\n";
+    print 'to : ' . $dest . "\n";
+    copy($src, $dest);
+    if(file_exists($dest)) {
+        print "move [OK] \n";
+    } else {
+        print "move [KO] \n";
+    }
+    unlink($src);
+    if(file_exists($src)) {
+        print "delete [KO] \n";
+    } else {
+        print "delete [OK] \n";
+    }
+}
